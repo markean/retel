2fb0260c654f3240b85caf77ef3ece5e18d2dc97
markean <markean@protonmail.com>
2024-01-01 00:15:12 -0500

    arxiv information updated

--- R/retel-package.R
+++ R/retel-package.R
@@ -1,7 +1,7 @@
 #' @references Kim E, MacEachern SN, Peruggia M (2023).
 #'   "Regularized Exponentially Tilted Empirical Likelihood for Bayesian
 #'   Inference."
-#'   \doi{10.48550/arXiv.2112.09206}.
+#'   \doi{10.48550/arXiv.2312.17015}.
 #' @keywords internal
 "_PACKAGE"
 
